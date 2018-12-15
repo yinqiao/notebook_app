6dc446e933aaaa1b1c9b2f077bb081549798edba
银翘 <yinqiao.zrj@alibaba-inc.com>
2018-12-15 15:35:26 +0800

    update

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+# introduction
+
+MongoDB + Node.JS + Express.JS system
+
+http://127.0.0.1:3001/
---
+++ client/src/App.js
@@ -0,0 +1,173 @@
+// /client/App.js
+import React, { Component } from "react";
+import axios from "axios";
+import Button from 'antd/lib/button';
+import './App.css';
+
+class App extends Component {
+  // initialize our state 
+  state = {
+    data: [],
+    id: 0,
+    message: null,
+    intervalIsSet: false,
+    idToDelete: null,
+    idToUpdate: null,
+    objectToUpdate: null
+  };
+
+  // when component mounts, first thing it does is fetch all existing data in our db
+  // then we incorporate a polling logic so that we can easily see if our db has 
+  // changed and implement those changes into our UI
+  componentDidMount() {
+    this.getDataFromDb();
+    if (!this.state.intervalIsSet) {
+      let interval = setInterval(this.getDataFromDb, 1000);
+      this.setState({ intervalIsSet: interval });
+    }
+  }
+
+  // never let a process live forever 
+  // always kill a process everytime we are done using it
+  componentWillUnmount() {
+    if (this.state.intervalIsSet) {
+      clearInterval(this.state.intervalIsSet);
+      this.setState({ intervalIsSet: null });
+    }
+  }
+
+  // just a note, here, in the front end, we use the id key of our data object 
+  // in order to identify which we want to Update or delete.
+  // for our back end, we use the object id assigned by MongoDB to modify 
+  // data base entries
+
+  // our first get method that uses our backend api to 
+  // fetch data from our data base
+  getDataFromDb = () => {
+    fetch("/api/getData")
+      .then(data => data.json())
+      .then(res => this.setState({ data: res.data }));
+  };
+
+  // our put method that uses our backend api
+  // to create new query into our data base
+  putDataToDB = message => {
+    let currentIds = this.state.data.map(data => data.id);
+    let idToBeAdded = 0;
+    while (currentIds.includes(idToBeAdded)) {
+      ++idToBeAdded;
+    }
+
+    axios.post("/api/putData", {
+      id: idToBeAdded,
+      message: message
+    });
+  };
+
+
+  // our delete method that uses our backend api 
+  // to remove existing database information
+  deleteFromDB = idTodelete => {
+    let objIdToDelete = null;
+    this.state.data.forEach(dat => {
+      if (dat.id == idTodelete) {
+        objIdToDelete = dat._id;
+      }
+    });
+
+    axios.delete("/api/deleteData", {
+      data: {
+        id: objIdToDelete
+      }
+    });
+  };
+
+  // our update method that uses our backend api
+  // to overwrite existing data base information
+  updateDB = (idToUpdate, updateToApply) => {
+    let objIdToUpdate = null;
+    this.state.data.forEach(dat => {
+      if (dat.id == idToUpdate) {
+        objIdToUpdate = dat._id;
+      }
+    });
+
+    axios.post("/api/updateData", {
+      id: objIdToUpdate,
+      update: { message: updateToApply }
+    });
+  };
+
+  // here is our UI
+  // it is easy to understand their functions when you 
+  // see them render into our screen
+  render() {
+    const { data } = this.state;
+    return (
+      <div>
+  
+        <ul>
+          {data.length <= 0
+            ? "无数据"
+            : data.map(dat => (
+                <li style={{ padding: "10px" }} key={data.message}>
+                  <span style={{ color: "gray" }}> id: </span> {dat.id} <br />
+                  <span style={{ color: "gray" }}> data: </span>
+                  {dat.message}
+                </li>
+              ))}
+        </ul>
+        <div style={{ padding: "10px" }}>
+          <input
+            type="text"
+            onChange={e => this.setState({ message: e.target.value })}
+            placeholder="请输入内容"
+            style={{ width: "200px" }}
+          />
+          <button onClick={() => this.putDataToDB(this.state.message)}>添加</button>
+        </div>
+        <div style={{ padding: "10px" }}>
+          <input
+            type="text"
+            style={{ width: "200px" }}
+            onChange={e => this.setState({ idToDelete: e.target.value })}
+            placeholder="填写所需删除的ID"
+          />
+          <button onClick={() => this.deleteFromDB(this.state.idToDelete)}>删除</button>
+        </div>
+        <div style={{ padding: "10px" }}>
+          <input
+            type="text"
+            style={{ width: "200px" }}
+            onChange={e => this.setState({ idToUpdate: e.target.value })}
+            placeholder="所需更新的ID"
+          />
+          <input
+            type="text"
+            style={{ width: "200px" }}
+            onChange={e => this.setState({ updateToApply: e.target.value })}
+            placeholder="请输入所需更新的内容"
+          />
+          <button
+            onClick={() =>
+              this.updateDB(this.state.idToUpdate, this.state.updateToApply)
+            }
+          >更新</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+// // client/src/App.js
+// import React, { Component } from "react";
+
+// class App extends Component {
+//   render() {
+//     return <div>欢迎来到笔记本应用!:-)</div>;
+//   }
+// }
+
+// export default App;
