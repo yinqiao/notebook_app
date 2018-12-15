2850a700423c69c4dbec4e2a3ad7a8ffe887e6d0
银翘 <yinqiao.zrj@alibaba-inc.com>
2018-12-16 00:12:49 +0800

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 // /client/App.js
 import React, { Component } from "react";
 import axios from "axios";
-import Button from 'antd/lib/button';
+import {
+  Button,
+  Input,
+  List,
+  Avatar,
+  Card,
+} from 'antd';
 import './App.css';
 
 class App extends Component {
-  // initialize our state 
+  // 初始化 state 
   state = {
     data: [],
     id: 0,
@@ -16,9 +22,8 @@ class App extends Component {
     objectToUpdate: null
   };
 
-  // when component mounts, first thing it does is fetch all existing data in our db
-  // then we incorporate a polling logic so that we can easily see if our db has 
-  // changed and implement those changes into our UI
+  // 首先从数据库中获取已有数据
+  // 然后添加轮询机制，用于检测数据库的数据，当数据发生更新时，重新渲染UI
   componentDidMount() {
     this.getDataFromDb();
     if (!this.state.intervalIsSet) {
@@ -27,8 +32,8 @@ class App extends Component {
     }
   }
 
-  // never let a process live forever 
-  // always kill a process everytime we are done using it
+  // 在 componentWillUnmount 时销毁定时器
+  // 需要及时销毁不需要使用的进程
   componentWillUnmount() {
     if (this.state.intervalIsSet) {
       clearInterval(this.state.intervalIsSet);
@@ -36,21 +41,16 @@ class App extends Component {
     }
   }
 
-  // just a note, here, in the front end, we use the id key of our data object 
-  // in order to identify which we want to Update or delete.
-  // for our back end, we use the object id assigned by MongoDB to modify 
-  // data base entries
-
-  // our first get method that uses our backend api to 
-  // fetch data from our data base
+  // 在前台使用ID作为数据的key来辨识所需更新或删除的数据
+  // 在后台使用ID作为MongoDB中的数据实例的修改依据
+  // getDataFromDb 函数用于从数据库中获取数据
   getDataFromDb = () => {
     fetch("/api/getData")
       .then(data => data.json())
       .then(res => this.setState({ data: res.data }));
   };
 
-  // our put method that uses our backend api
-  // to create new query into our data base
+  // putDataToDB 函数用于调用后台API接口向数据库新增数据
   putDataToDB = message => {
     let currentIds = this.state.data.map(data => data.id);
     let idToBeAdded = 0;
@@ -65,8 +65,7 @@ class App extends Component {
   };
 
 
-  // our delete method that uses our backend api 
-  // to remove existing database information
+  // deleteFromDB函数用于调研后台API删除数据库中已经存在的数据
   deleteFromDB = idTodelete => {
     let objIdToDelete = null;
     this.state.data.forEach(dat => {
@@ -82,8 +81,7 @@ class App extends Component {
     });
   };
 
-  // our update method that uses our backend api
-  // to overwrite existing data base information
+  // updateDB 函数用于调用后台API更新数据库中已经存在的数据
   updateDB = (idToUpdate, updateToApply) => {
     let objIdToUpdate = null;
     this.state.data.forEach(dat => {
@@ -98,62 +96,74 @@ class App extends Component {
     });
   };
 
-  // here is our UI
-  // it is easy to understand their functions when you 
-  // see them render into our screen
+  // 渲染UI的核心方法
+  // 该渲染函数渲染的内容与前台界面展示一致
   render() {
-    const { data } = this.state;
+    const { data = [] } = this.state;
+    console.log('data', data)
     return (
-      <div>
-  
-        <ul>
-          {data.length <= 0
-            ? "无数据"
-            : data.map(dat => (
-                <li style={{ padding: "10px" }} key={data.message}>
-                  <span style={{ color: "gray" }}> id: </span> {dat.id} <br />
-                  <span style={{ color: "gray" }}> data: </span>
-                  {dat.message}
-                </li>
-              ))}
-        </ul>
-        <div style={{ padding: "10px" }}>
-          <input
-            type="text"
+      <div style={{ width: 990, margin: 20 }}>
+        <List
+          itemLayout="horizontal"
+          dataSource={data}
+          renderItem={item => (
+            <List.Item>
+              <List.Item.Meta
+                avatar={<Avatar src="https://gw.alicdn.com/tfs/TB1Hup.wa6qK1RjSZFmXXX0PFXa-1024-1024.jpg" />}
+                title={<span>{`创建时间：${item.createdAt}`}</span>}
+                description={`${item.id}: ${item.message}`}
+              />
+            </List.Item>
+          )}
+        />
+        <Card
+          title="新增笔记" 
+          style={{ padding: 10, margin: 10 }}>
+          <Input
             onChange={e => this.setState({ message: e.target.value })}
-            placeholder="请输入内容"
-            style={{ width: "200px" }}
-          />
-          <button onClick={() => this.putDataToDB(this.state.message)}>添加</button>
-        </div>
-        <div style={{ padding: "10px" }}>
-          <input
-            type="text"
+            placeholder="请输入笔记内容"
+            style={{ width: 200 }} />
+          <Button
+            type="primary" 
+            style={{ margin: 20 }}
+            onClick={() => this.putDataToDB(this.state.message)}
+          >添加</Button>
+        </Card>
+        <Card
+          title="删除笔记" 
+          style={{ padding: 10, margin: 10 }}>
+          <Input
             style={{ width: "200px" }}
             onChange={e => this.setState({ idToDelete: e.target.value })}
             placeholder="填写所需删除的ID"
           />
-          <button onClick={() => this.deleteFromDB(this.state.idToDelete)}>删除</button>
-        </div>
-        <div style={{ padding: "10px" }}>
-          <input
-            type="text"
-            style={{ width: "200px" }}
-            onChange={e => this.setState({ idToUpdate: e.target.value })}
+          <Button
+            type="primary" 
+            style={{ margin: 20 }}
+            onClick={() => this.deleteFromDB(this.state.idToDelete)}
+          >删除</Button>
+        </Card>
+        <Card
+          title="更新笔记" 
+          style={{ padding: 10, margin: 10 }}>
+          <Input
+            style={{ width: 200, marginRight: 10 }}
             placeholder="所需更新的ID"
-          />
-          <input
-            type="text"
-            style={{ width: "200px" }}
+            onChange={e => this.setState({ idToUpdate: e.target.value })}
+            />
+          <Input
+            style={{ width: 200 }}
             onChange={e => this.setState({ updateToApply: e.target.value })}
             placeholder="请输入所需更新的内容"
           />
-          <button
+          <Button
+            type="primary" 
+            style={{ margin: 20 }}
             onClick={() =>
               this.updateDB(this.state.idToUpdate, this.state.updateToApply)
             }
-          >更新</button>
-        </div>
+          >更新</Button>
+        </Card>
       </div>
     );
   }
